4c14ac9c77617922cf552bbd3b2683576ad5bca0
pstuever27 <pstuever27@gmail.com>
2021-09-14 12:57:51 -0500

    update?

--- Lab02 Shapes/main.cpp
+++ Lab02 Shapes/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char* argv[])
 {
-    Shape Run;
-    
+  //takes the filename from the argv
+  std::string fileName;
+  if(argc > 1)
+  {
+    fileName = argv[1];
+  //makes menu object and begins the program by calling .run
+    Shape run;
+    run.run(fileName);
+  }
+  else
+  {
+    //check for invalid file in argv
+    std::cout << "Invalid Filename!\n";
+  }
+  return(0);
+}
+
 }
