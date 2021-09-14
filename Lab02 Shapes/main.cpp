1c9adc17ff332b8bb48072aaa0da87e3a2af9515
pstuever27 <pstuever27@gmail.com>
2021-09-14 11:22:08 -0500

    updates

--- Lab02 Shapes/main.cpp
+++ Lab02 Shapes/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
-
+#include "shape.h"
+#include "ShapeContainer.h"
 int main(int argc, char* argv[])
 {
+    std::string fileName;
+    if(argc > 1)
+    {
+        fileName = argv[1];
+        Shape run;
+        run.
+    }
     Shape Run;
     
 }
