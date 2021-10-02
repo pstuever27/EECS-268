059c8f3dfc0dd80291b0cdf8cf0933f69290e170
pstuever27 <pstuever27@gmail.com>
2021-10-01 22:39:55 -0500

    Added algorithm to check action from file

--- Lab04 Elevator Action!/Executive.cpp
+++ Lab04 Elevator Action!/Executive.cpp
@@ -25,6 +25,65 @@ void Executive::run()
     while(inFile >> action)
     {
       inFile >> name;
+      if(action == "WAIT")
+      {
+        Line = Line->enqueue(name);
+      }
+      else if(action == "PICK_UP")
+      {
+        int i = 0;
+        while(i<7)
+        {
+          try
+          {
+            Elevator = Line->peekFront();
+            Line = Line->dequeue();
+            i++;
+          }
+          catch(std::exception& e)
+          {
+            break;
+          }
+        }
+      }
+      else if(action == "DROP_OFF")
+      {
+        int i = 0;
+        while(i<7)
+        {
+          try
+          {
+            Elevator = Elevator->pop();
+          }
+          catch(const std::exception& e)
+          {
+            break;
+          }
+        }
+      }
+      else if(action == "INSPECTION")
+      {
+        bool empty;
+        std::string elevator_next;
+        std::string Line_next;
+        empty = Elevator->isEmpty();
+        
+      
+        elevator_next = Elevator->peek();
+        Line_next = Line->peekFront();
+
+          if(empty)
+          {
+            std::cout << "The elevator is empty\n";
+          }
+          else
+          {
+            std::cout << "The elevator is not empty\n";
+            std::cout << elevator_next << " is the next person to get off the elevator\n";
+          }
+        
+        std::cout << Line_next << " is the next person to get on the elevator\n";
+      }
     }
   }
   else
